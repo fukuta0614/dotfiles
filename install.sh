c417349fb5eaf18e848379306b0c7bc2300d30cb
fukuta0614 <fukuta6140@gmail.com>
2017-05-30 03:20:23 +0900

    delete antigen

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ for f in .??*
   do
     [[ "$f" == ".git" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
+    [[ "$f" == ".gitignore" ]] && continue
     [ -f ~/$f ] && mv ~/$f ~/dotfiles/oldfiles/
     ln -s ~/dotfiles/$f ~/$f 
   done
