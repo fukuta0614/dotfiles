3636a14076c52c440b38eb282dcf2fed339d35f3
fukuta0614 <fukuta6140@gmail.com>
2017-08-10 18:12:21 +0900

    add gitkeep

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-mkdir oldfiles
+
 for f in .??*
   do
     [[ "$f" == ".git" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
     [[ "$f" == ".gitignore" ]] && continue
     [[ "$f" == ".zshrc.mil" ]] && continue
-    [ -f ~/$f ] && mv ~/$f ~/dotfiles/oldfiles/
-    ln -s ~/dotfiles/$f ~/$f 
+    [ -f ~/$f ] && mv ~/$f ~/.dotfiles/oldfiles/
+    ln -s ~/.dotfiles/$f ~/$f 
   done
